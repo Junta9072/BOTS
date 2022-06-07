37a9a3044e52d713983e32db8fd70e5c129c0089
unknown <yarne.brangers@telenet.be>
2022-06-07 16:29:22 +0200

    added threeJS, texture not working locally, test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import * as THREE from 'three';
+
 //remove gravity from acl
 let noGrav = { x: 0, y: 0, z: 0 };
 function noGravAcl() {
@@ -47,3 +49,56 @@ gravitySensor.addEventListener('reading', (e) => {
 gravitySensor.start();
 acl.start();
 gyroscope.start();
+
+//3js
+
+const scene = new THREE.Scene();
+const loader = new THREE.TextureLoader();
+scene.background = new THREE.Color(0xffffff);
+
+//verlichting
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+directionalLight.position.set(200, 500, 300);
+scene.add(directionalLight);
+
+//camera
+// Setting up camera
+const aspectRatio = 1 / 1;
+const cameraWidth = 150;
+const cameraHeight = cameraWidth / aspectRatio;
+
+const camera = new THREE.OrthographicCamera(
+  cameraWidth / -2, // left
+  cameraWidth / 2, // right
+  cameraHeight / 2, // top
+  cameraHeight / -2, // bottom
+  0, // near plane
+  1000, // far plane
+);
+camera.position.set(200, 200, 200);
+camera.lookAt(0, 10, 0);
+
+// Set up renderer
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(200, 200);
+renderer.render(scene, camera);
+
+document.body.appendChild(renderer.domElement);
+
+function createPhone() {
+  const geometry = new THREE.BoxBufferGeometry(14, 28, 2);
+  const material = new THREE.MeshLambertMaterial({
+    color: 0xffbe0b,
+    map: loader.load('textures/cover.jpg'),
+  });
+  const phone = new THREE.Mesh(geometry, material);
+  phone.position.x = 0;
+  phone.position.y = 0;
+  scene.add(phone);
+}
+createPhone();
+
+renderer.render(scene, camera);
