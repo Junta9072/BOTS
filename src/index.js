d7400b5160aade38cdde47cb887b3cab03d977d4
unknown <yarne.brangers@telenet.be>
2022-06-10 12:07:52 +0200

    23rd quaternion test

--- src/index.js
+++ src/index.js
@@ -132,7 +132,14 @@ loadManager.onLoad = () => {
     renderer.render(scene, camera);
   });*/
 
-  sensor.onreading = 
-  () => phone.quaternion.fromArray(sensor.quaternion);
-  renderer.render(scene, camera);
+  let torusGeometry = new THREE.TorusGeometry(7, 1.6, 4, 3, 6.3);
+  let material = new THREE.MeshBasicMaterial({ color: 0x0071c5 });
+  let torus = new THREE.Mesh(torusGeometry, material);
+  scene.add(torus);
+  
+  // Update mesh rotation using quaternion.
+  const sensorAbs = new AbsoluteOrientationSensor();
+  sensorAbs.onreading = () => torus.quaternion.fromArray(sensorAbs.quaternion);
+  sensorAbs.start();
+  
 };
